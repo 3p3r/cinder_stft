c29c18d10fd88538da736a57e81394ba192ff5b2
Sepehr Laal <sepehr.laal@gmail.com>
2015-02-02 19:53:54 -0800

    Checked in boilerplate for app_events

---
+++ include/app_event.h
@@ -0,0 +1,19 @@
+#ifndef CIEQ_INCLUDE_APP_EVENTS_H_
+#define CIEQ_INCLUDE_APP_EVENTS_H_
+
+namespace cieq
+{
+
+/*!
+ * \class AppEvents
+ * \brief The class that handles mouse and keyboard input,
+ * and potentially other physical inputs (not audio tho!)
+ */
+class AppEvents
+{
+
+};
+
+} //!cieq
+
+#endif //!CIEQ_INCLUDE_APP_EVENTS_H_
