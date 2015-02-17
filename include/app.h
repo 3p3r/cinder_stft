7b4973749f336812f791a213343c1329dd664e03
Sepehr Laal <sepehr.laal@gmail.com>
2015-02-17 12:43:55 -0800

    Documented app.h

--- include/app.h
+++ include/app.h
@@ -22,31 +22,45 @@ class InputAnalyzer final : public ci::app::AppNative
 public:
 	InputAnalyzer();
 
-	// \note Tells Cinder how to prepare the window
-	void		prepareSettings(Settings *settings) override final;
-
-	void		setup() override final;
-	void		update() override final;
-	void		draw() override final;
-	void		resize() override final;
-	void		shutdown() override final;
-	void		positionPlots();
-	void		drawFps();
-
-	void		mouseDown(ci::app::MouseEvent event) override final;
-	void		mouseDrag(ci::app::MouseEvent event) override final;
-	void		keyDown(ci::app::KeyEvent event) override final;
-
-	AppGlobals&	getGlobals() { return mGlobals; }
+	//! tells Cinder how to prepare the window
+	void			prepareSettings(Settings *settings) override final;
+	//! sets up the application, this gets fired after Cinder is fully initialized.
+	void			setup() override final;
+	//! called at the beginning of every render loop, used to refresh data.
+	void			update() override final;
+	//! called at the end of every render loop, used to draw the data.
+	void			draw() override final;
+	//! application window resize callback.
+	void			resize() override final;
+	//! draws the current refresh rate of the render loop.
+	void			drawFps();
+	//! gets fired on mouse click
+	void			mouseDown(ci::app::MouseEvent event) override final;
+	//! gets fired on mouse drag
+	void			mouseDrag(ci::app::MouseEvent event) override final;
+	//! gets fired on keyboard click
+	void			keyDown(ci::app::KeyEvent event) override final;
 
 private:
+	//! repositions the plots after a resize is fired.
+	void			positionPlots();
+
+private:
+	//! the event processor class instance
 	AppEvent		mEventProcessor;
+	//! the globals class instance, used to store common data
 	AppGlobals		mGlobals;
+	//! camera rig class instance
 	AppCamera		mCameras;
+	//! audio I/O class instance
 	AudioNodes		mAudioNodes;
+	//! spectrum of the current window plot
 	SpectrumPlot	mSpectrumPlot;
+	//! plot of the current window's raw input
 	WaveformPlot	mWaveformPlot;
+	//! entire spectrogram plot instance
 	SpectrogramPlot	mSpectrogramPlot;
+	//! cinder's param ref, for tweaking variables at the runtime
 	std::shared_ptr<ci::params::InterfaceGl>
 					mParamsRef;
 };
