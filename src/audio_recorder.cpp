c7c70f39d5c2e48192144e36447c324a117fb90a
Sepehr Laal <sepehr.laal@gmail.com>
2015-02-21 17:28:58 -0800

    Wrapped FMOD code inside separate classes

--- src/audio_recorder.cpp
+++ src/audio_recorder.cpp
@@ -3,39 +3,194 @@
 #include <fmod.hpp>
 #include <fmod_errors.h>
 
-#include <exception>
+#include <stdexcept>
 #include <thread>
 
 namespace cieq
 {
 
-struct FmodWrapper : public std::enable_shared_from_this<FmodWrapper>
+/*!
+ * \struct FmodWrapper
+ * \private
+ * \brief A thin wrapper over the FMOD sound engine, designed for
+ * audio recording via microphone / other physical inputs.
+ * \note this class is not exposed publicly simply because the
+ * entire FMOD functionality is not needed.
+ */
+struct FmodWrapper final : public std::enable_shared_from_this<FmodWrapper>
 {
-	FMOD::System*			mSystem = 0;
-	FMOD::Sound*			mSound = 0;
-	FMOD_RESULT				mResult = FMOD_OK;
-	unsigned int			mVersion = 0;
-	unsigned int			mSoundLength = 0;
-	unsigned int			mRecordPos = 0;
-	int						mRecordRate = 0;
-	int						mRecordChannels = 0;
-	int						mRecordNumDrivers = 0;
-	FMOD_CREATESOUNDEXINFO	mExInfo;
-	void					checkFmodError(FMOD_RESULT result) const
+	//! \brief a C++ exception utility for throwing FMOD errors.
+	//! \note provides construction by FMOD_RESULT.
+	class FmodWrapperException final : public std::runtime_error
 	{
-		if (result != FMOD_OK)
+	public:
+		explicit FmodWrapperException(FMOD_RESULT code)
+			: std::runtime_error(FMOD_ErrorString(code))
+		{}
+		explicit FmodWrapperException(const std::string& what)
+			: std::runtime_error(what)
+		{}
+		explicit FmodWrapperException(const char* what)
+			: std::runtime_error(what)
+		{}
+	};
+
+	//! \brief A convenience for wrapping FMOD's ExInfo options in a class
+	class FmodWrapperExInfoOptions
+	{
+		//! \note totally a convenience, don't want my line lengths exceed my text editor.
+		using Self = FmodWrapperExInfoOptions;
+
+	public:
+		Self&				setFormat(FMOD_SOUND_FORMAT fmt) { mFormat = fmt; return *this; }
+		Self&				setFrequency(int freq) { mFrequency = freq; return *this; }
+		Self&				setLength(std::size_t len) { mLength = len; return *this; }
+
+		FMOD_SOUND_FORMAT	getFormat() const { return mFormat; }
+		int					getFrequency() const { return mFrequency; }
+		std::size_t			getLength() const { return mLength; }
+
+	private:
+		FMOD_SOUND_FORMAT	mFormat{ FMOD_SOUND_FORMAT_PCMFLOAT };
+		int					mFrequency{ 0 };
+		std::size_t			mLength{ 0 };
+	};
+
+	//! \brief takes care of FMOD's system initialization and
+	//! version checking stuff.
+	FmodWrapper()
+	{
+		mResult = FMOD::System_Create(&mSystem);
+		checkLastResult();
+
+		mResult = mSystem->getVersion(&mVersion);
+		checkLastResult();
+
+		if (mVersion < FMOD_VERSION)
 		{
-			const std::string msg(FMOD_ErrorString(result));
-			throw std::exception(msg.c_str());
+			throw FmodWrapperException("FMOD lib version doesn't match header version.");
+		}
+
+		initializeSystem();
+		getDriverInfo();
+		zeroOutExInfo();
+	}
+
+	void					setup(const FmodWrapperExInfoOptions& opts)
+	{
+		setupExInfo(opts);
+		setupSound();
+	}
+
+	void					startRecording()
+	{
+		if (!isRecording())
+		{
+			mResult = mSystem->recordStart(0, mSound, false);
+			checkLastResult();
 		}
 	}
+
+	void					stopRecording()
+	{
+		if (isRecording())
+		{
+			mResult = mSystem->recordStop(0);
+			checkLastResult();
+		}
+	}
+
+	bool					isRecording()
+	{
+		mSystem->isRecording(0, &mRecordStatus);
+		return mRecordStatus;
+	}
+
+	void					update()
+	{
+		mResult = mSystem->update();
+		checkLastResult();
+	}
+
 	~FmodWrapper()
 	{
-		auto result = mSound->release();
-		checkFmodError(result);
-		result = mSystem->release();
-		checkFmodError(result);
+		if (mSound)
+		{
+			mResult = mSound->release();
+			checkLastResult();
+		}
+
+		if (mSystem)
+		{
+			mResult = mSystem->release();
+			checkLastResult();
+		}
 	}
+
+private:
+	void					checkLastResult() const
+	{
+		if (mResult != FMOD_OK)
+		{
+			throw FmodWrapperException(mResult);
+		}
+	}
+
+	void					initializeSystem()
+	{
+		mResult = mSystem->init(100, FMOD_INIT_NORMAL, nullptr);
+		checkLastResult();
+	}
+
+	void					setupExInfo(const FmodWrapperExInfoOptions& opts)
+	{
+		mExInfo.cbsize = sizeof(FMOD_CREATESOUNDEXINFO);
+		mExInfo.numchannels = mRecordChannels;
+		mExInfo.format = opts.getFormat();
+		mExInfo.defaultfrequency = opts.getFrequency();
+		mExInfo.length = mExInfo.defaultfrequency * sizeof(float) * mExInfo.numchannels * opts.getLength();
+	}
+
+	void					setupSound()
+	{
+		mResult = mSystem->createSound(0, FMOD_LOOP_NORMAL | FMOD_OPENUSER, &mExInfo, &mSound);
+		checkLastResult();
+
+		mResult = mSound->getLength(&mSoundLength, FMOD_TIMEUNIT_PCM);
+		checkLastResult();
+	}
+
+	void					getDriverInfo()
+	{
+		mResult = mSystem->getRecordNumDrivers(&mRecordNumDrivers);
+		checkLastResult();
+
+		if (mRecordNumDrivers <= 0)
+		{
+			throw FmodWrapperException("No recording devices found/plugged in!  Aborting.");
+		}
+
+		mResult = mSystem->getRecordDriverInfo(0, NULL, 0, 0, &mRecordRate, 0, &mRecordChannels);
+		checkLastResult();
+	}
+
+	void					zeroOutExInfo()
+	{
+		std::memset(&mExInfo, 0, sizeof(FMOD_CREATESOUNDEXINFO));
+	}
+
+public:
+	FMOD::System*			mSystem{ nullptr };
+	FMOD::Sound*			mSound{ nullptr };
+	FMOD_RESULT				mResult{ FMOD_OK };
+	unsigned int			mVersion{ 0 };
+	unsigned int			mSoundLength{ 0 };
+	unsigned int			mRecordPos{ 0 };
+	int						mRecordRate{ 0 };
+	int						mRecordChannels{ 0 };
+	int						mRecordNumDrivers{ 0 };
+	bool					mRecordStatus{ false };
+	FMOD_CREATESOUNDEXINFO	mExInfo;
 };
 
 AudioRecorder::Options::Options()
@@ -80,75 +235,20 @@ bool AudioRecorder::Options::getAutoStart() const
 	return mAutoStart;
 }
 
-
 AudioRecorder::AudioRecorder(const Options& opts /*= Options()*/)
 	: mOpts(opts)
 {
 	auto fmod_wrapper = std::make_shared<FmodWrapper>();
-	/*
-	Create a System object and initialize.
-	*/
-	fmod_wrapper->mResult = FMOD::System_Create(&fmod_wrapper->mSystem);
-	fmod_wrapper->checkFmodError(fmod_wrapper->mResult);
-
-	fmod_wrapper->mResult = fmod_wrapper->mSystem->getVersion(&fmod_wrapper->mVersion);
-	fmod_wrapper->checkFmodError(fmod_wrapper->mResult);
-
-	if (fmod_wrapper->mVersion < FMOD_VERSION)
-	{
-		throw std::exception("FMOD lib version doesn't match header version.");
-	}
-
-	fmod_wrapper->mResult = fmod_wrapper->mSystem->init(100, FMOD_INIT_NORMAL, nullptr);
-	fmod_wrapper->checkFmodError(fmod_wrapper->mResult);
-
-	fmod_wrapper->mResult = fmod_wrapper->mSystem->getRecordNumDrivers(&fmod_wrapper->mRecordNumDrivers);
-	fmod_wrapper->checkFmodError(fmod_wrapper->mResult);
-
-	if (fmod_wrapper->mRecordNumDrivers <= 0)
-	{
-		throw std::exception("No recording devices found/plugged in!  Aborting.");
-	}
-
-	fmod_wrapper->mResult = fmod_wrapper->mSystem->getRecordDriverInfo(0, NULL, 0, 0, &fmod_wrapper->mRecordRate, 0, &fmod_wrapper->mRecordChannels);
-	fmod_wrapper->checkFmodError(fmod_wrapper->mResult);
-
-	/*
-	Create user sound to record into.
-	*/
-	memset(&fmod_wrapper->mExInfo, 0, sizeof(FMOD_CREATESOUNDEXINFO));
-
-	fmod_wrapper->mExInfo.cbsize = sizeof(FMOD_CREATESOUNDEXINFO);
-	fmod_wrapper->mExInfo.numchannels = fmod_wrapper->mRecordChannels;
-	fmod_wrapper->mExInfo.format = FMOD_SOUND_FORMAT_PCMFLOAT;
-	fmod_wrapper->mExInfo.defaultfrequency = mOpts.getSampleRate();
-	fmod_wrapper->mExInfo.length = fmod_wrapper->mExInfo.defaultfrequency * sizeof(float) * fmod_wrapper->mExInfo.numchannels * mOpts.getDuration();
-
-	fmod_wrapper->mResult = fmod_wrapper->mSystem->createSound(0, FMOD_LOOP_NORMAL | FMOD_OPENUSER, &fmod_wrapper->mExInfo, &fmod_wrapper->mSound);
-	fmod_wrapper->checkFmodError(fmod_wrapper->mResult);
-
-	fmod_wrapper->mResult = fmod_wrapper->mSound->getLength(&fmod_wrapper->mSoundLength, FMOD_TIMEUNIT_PCM);
-	fmod_wrapper->checkFmodError(fmod_wrapper->mResult);
+	
+	fmod_wrapper->setup(FmodWrapper::FmodWrapperExInfoOptions()
+		.setFormat(FMOD_SOUND_FORMAT_PCMFLOAT)
+		.setFrequency(opts.getSampleRate())
+		.setLength(opts.getDuration()));
 
 	if (mOpts.getAutoStart())
 	{
-		fmod_wrapper->mResult = fmod_wrapper->mSystem->recordStart(0, fmod_wrapper->mSound, false);
-		fmod_wrapper->checkFmodError(fmod_wrapper->mResult);
+		fmod_wrapper->startRecording();
 	}
-
-	/*bool is_recording = false;
-	mSystem->isRecording(0, &is_recording);
-
-	do
-	{
-		mResult = mSystem->update();
-		checkFmodError(mResult);
-
-		mSystem->isRecording(0, &is_recording);
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
-
-	} while (is_recording);*/
 
 	mFmodWrapper = fmod_wrapper->shared_from_this();
 }
