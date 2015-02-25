239a92f86c9415b298010cbe18bdb0ddca0d7604
Sepehr Laal <sepehr.laal@gmail.com>
2015-02-24 22:16:30 -0800

    Added base of stft client / request headers

---
+++ include/stft_client.h
@@ -0,0 +1,16 @@
+#ifndef CIEQ_INCLUDE_STFT_CLIENT_H_
+#define CIEQ_INCLUDE_STFT_CLIENT_H_
+
+#include "work_client.h"
+
+namespace cieq {
+namespace stft {
+
+class Client : public work::Client
+{
+
+};
+
+}} // !namespace cieq::stft
+
+#endif // !CIEQ_INCLUDE_STFT_CLIENT_H_
---
+++ include/stft_request.h
@@ -0,0 +1,16 @@
+#ifndef CIEQ_INCLUDE_STFT_REQUEST_H_
+#define CIEQ_INCLUDE_STFT_REQUEST_H_
+
+#include "work_request.h"
+
+namespace cieq {
+namespace stft {
+
+class Request : public work::Request
+{
+
+};
+
+}} // !namespace cieq::stft
+
+#endif // !CIEQ_INCLUDE_STFT_REQUEST_H_
