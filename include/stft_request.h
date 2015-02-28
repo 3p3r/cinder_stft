59081aba86aca72e5df169a347ecd3af202101ee
Sepehr Laal <sepehr.laal@gmail.com>
2015-02-27 20:22:12 -0800

    Basic stft work request implemented

--- include/stft_request.h
+++ include/stft_request.h
@@ -8,7 +8,12 @@ namespace stft {
 
 class Request : public work::Request
 {
+public:
+	Request(std::size_t query_pos) : mQueryPos(query_pos) {}
+	std::size_t getQueryPos() const { return mQueryPos; }
 
+private:
+	std::size_t mQueryPos;
 };
 
 }} // !namespace cieq::stft
