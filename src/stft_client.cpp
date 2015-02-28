070408d0477e6a44ffb17ebf27b5be34a5df62d4
Sepehr Laal <sepehr.laal@gmail.com>
2015-02-27 18:13:17 -0800

    Minor adjustments to TLS of stft_client

--- src/stft_client.cpp
+++ src/stft_client.cpp
@@ -24,14 +24,15 @@ static class ClientResourcesAllocator
 public:
 	void allocate(std::size_t fft_size, std::size_t window_size, ClientResources& local_rsc) {
 		std::lock_guard<std::mutex> _lock(mResourceLock);
-		mPrivateMemory.push_back(std::make_pair(ci::audio::dsp::Fft(fft_size), ci::audio::makeAlignedArray<float>(window_size)));
-		local_rsc.mFftProcessor = &mPrivateMemory.back().first;
+		mPrivateMemory.push_back(std::make_pair(std::make_unique<ci::audio::dsp::Fft>(fft_size), ci::audio::makeAlignedArray<float>(window_size)));
+		local_rsc.mFftProcessor = mPrivateMemory.back().first.get();
 		local_rsc.mWindowingTable = &mPrivateMemory.back().second;
 	}
 
 private:
 	std::mutex	mResourceLock;
-	std::vector < std::pair< ci::audio::dsp::Fft, ci::audio::AlignedArrayPtr > >
+	/* mind: blown. */
+	std::vector < std::pair< std::unique_ptr< ci::audio::dsp::Fft >, ci::audio::AlignedArrayPtr > >
 				mPrivateMemory;
 } _resources_allocator;
 
@@ -44,11 +45,13 @@ Client::Client(work::Manager& m, Format fmt /*= Format()*/)
 
 void Client::handle(work::RequestRef)
 {
+	// Allocate once per thread.
 	thread_local static bool _ready = false;
 	if (!_ready)
 	{
 		// pass thread's local storage to the allocator function
 		_resources_allocator.allocate(mFormat.getFftSize(), mFormat.getWindowSize(), _resources);
+		_ready = true;
 	}
 }
 
