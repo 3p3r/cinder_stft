6c5746ba667d5d65fbbe1ab7bd8d7b6f6a9d2dfd
Sepehr Laal <sepehr.laal@gmail.com>
2015-02-28 20:55:51 -0800

    more logic on thread renderer

--- include/thread_renderer.h
+++ include/thread_renderer.h
@@ -13,16 +13,18 @@ class AudioNodes;
 class ThreadRenderer
 {
 public:
-	ThreadRenderer(AudioNodes&);
+	ThreadRenderer(AudioNodes&, int frames_per_unit, int fft_size, int viewable_frames);
 
 	void								update();
 	void								draw();
 	SpectralSurface&					getSurface(int index);
+	ci::gl::Texture&					getTexture(int index);
 
 private:
-	std::vector<SpectralSurface>		mSurfacePool;
+	std::vector<SpectralSurfaceRef>		mSurfacePool;
 	std::vector<ci::gl::TextureRef>		mTexturePool;
 	ci::gl::Fbo							mFramebuffer;
+	int									mViewableFrames;
 };
 
 } // !namespace cieq
--- src/thread_renderer.cpp
+++ src/thread_renderer.cpp
@@ -1,25 +1,71 @@
 #include "thread_renderer.h"
+#include "audio_nodes.h"
+#include "recorder_node.h"
 
 namespace cieq {
 
-ThreadRenderer::ThreadRenderer(AudioNodes& nodes)
+ThreadRenderer::ThreadRenderer(AudioNodes& nodes, int frames_per_unit, int fft_size, int viewable_frames)
+	: mViewableFrames(viewable_frames)
 {
-	//TODO: used audio nodes to resize internal pools
+	int num_blocks = (nodes.getBufferRecorderNode()->getNumFrames() / frames_per_unit) + 1;
+	mSurfacePool.resize(num_blocks);
+	mTexturePool.resize(num_blocks);
+	for (auto index = 0; index < num_blocks; ++index)
+	{
+		mSurfacePool.push_back(std::make_unique<SpectralSurface>(frames_per_unit, fft_size));
+		mTexturePool.push_back(ci::gl::Texture::create(*mSurfacePool.back()));
+	}
+
+	mFramebuffer = ci::gl::Fbo(viewable_frames, fft_size);
 }
 
 void ThreadRenderer::update()
 {
-
+	auto index = 0;
+	for (SpectralSurfaceRef& surface : mSurfacePool)
+	{
+		if (*surface && surface->allRowsTouched())
+		{
+			mTexturePool[index]->update(*surface);
+			surface->reset();
+		}
+		++index;
+	}
 }
 
 void ThreadRenderer::draw()
 {
+	auto index = 0;
 
+	ci::gl::SaveFramebufferBinding _bindings;
+	
+	//! draw everything to a frame buffer
+	mFramebuffer.bindFramebuffer();
+
+	for (SpectralSurfaceRef& surface : mSurfacePool)
+	{
+		if (*surface)
+		{
+			//draw surface here.
+		}
+		else
+		{
+			//draw mTexturePool[index] here.
+		}
+		++index;
+	}
+
+	mFramebuffer.unbindFramebuffer();
 }
 
 SpectralSurface& ThreadRenderer::getSurface(int index)
 {
-	return mSurfacePool[index];
+	return *(mSurfacePool[index]);
+}
+
+ci::gl::Texture& ThreadRenderer::getTexture(int index)
+{
+	return *mTexturePool[index].get();
 }
 
 } //!cieq
