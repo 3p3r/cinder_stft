449c69fd69da9140d7541fa7abfe3aa833335fb8
Sepehr Laal <sepehr.laal@gmail.com>
2015-03-02 21:28:12 -0800

    Added Matlab's Jet pallete

--- src/smart_surface.cpp
+++ src/smart_surface.cpp
@@ -4,6 +4,39 @@
 
 namespace cieq {
 
+namespace {
+//! A Matlab "Jet" palleted color composition implementation, adapted to Cinder.
+//! \note original work: http://paulbourke.net/texture_colour/colourspace/
+//! \note taken from: http://stackoverflow.com/a/7811134/1055628
+static inline ci::Color GetColour(float v, float vmin, float vmax)
+{
+	ci::Color c{ ci::Color::white() }; // white
+	float dv;
+
+   if (v < vmin)
+      v = vmin;
+   if (v > vmax)
+      v = vmax;
+   dv = vmax - vmin;
+
+   if (v < (vmin + 0.25f * dv)) {
+      c.r = 0.0f;
+      c.g = 4.0f * (v - vmin) / dv;
+   } else if (v < (vmin + 0.5f * dv)) {
+      c.r = 0.0f;
+      c.b = 1.0f + 4.0f * (vmin + 0.25f * dv - v) / dv;
+   } else if (v < (vmin + 0.75f * dv)) {
+      c.r = 4.0f * (v - vmin - 0.5f * dv) / dv;
+      c.b = 0.0f;
+   } else {
+      c.g = 1.0f + 4.0f * (vmin + 0.75f * dv - v) / dv;
+      c.b = 0.0f;
+   }
+
+   return c;
+}
+}
+
 SpectralSurface::SpectralSurface(int width, int height)
 	: SmartSurface32f(width, height)
 {}
@@ -18,10 +51,11 @@ void SpectralSurface::processRow(int row, const std::vector<float>& spectrum)
 	//! color logic goes here
 	while (surface_iter.pixel())
 	{
-		auto m = ci::audio::linearToDecibel(spectrum[surface_iter.mX]) / 100;
-		surface_iter.r() = m;
-		surface_iter.g() = m;
-		surface_iter.b() = 1.0f - m;
+		auto m = 1024.0f * spectrum[surface_iter.mX];
+		auto c = GetColour(m, 0.0f, 1.0f);
+		surface_iter.r() = c.r;
+		surface_iter.g() = c.g;
+		surface_iter.b() = c.b;
 	}
 }
 
