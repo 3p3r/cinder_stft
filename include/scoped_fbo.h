21d3bf7d3556dd4a8113c0907c9faf8c801cee26
Sepehr Laal <sepehr.laal@gmail.com>
2015-03-06 21:13:16 -0800

    Added scoped_fbo class

---
+++ include/scoped_fbo.h
@@ -0,0 +1,28 @@
+#ifndef CIEQ_INCLUDE_SCOPED_FBO_H_
+#define CIEQ_INCLUDE_SCOPED_FBO_H_
+
+#include <cinder/Area.h>
+#include <cinder/gl/gl.h>
+
+namespace cinder {
+namespace gl {
+class Fbo;
+}} //!ci::gl
+
+namespace cieq {
+
+class ScopedFramebuffer
+{
+public:
+	ScopedFramebuffer(cinder::gl::Fbo&);
+	~ScopedFramebuffer();
+
+private:
+	cinder::gl::Fbo&				mFbo;
+	ci::Area						mOriginalViewport;
+	ci::gl::SaveFramebufferBinding	mSaveFb;
+};
+
+} // !namespace cieq
+
+#endif // !CIEQ_INCLUDE_SCOPED_FBO_H_
---
+++ src/scoped_fbo.cpp
@@ -0,0 +1,30 @@
+#include "scoped_fbo.h"
+
+#include <cinder/gl/Fbo.h>
+#include <cinder/Camera.h>
+
+namespace cieq {
+
+ScopedFramebuffer::ScopedFramebuffer(cinder::gl::Fbo& fbo)
+	: mFbo(fbo)
+	, mOriginalViewport(ci::gl::getViewport())
+{
+	mFbo.bindFramebuffer();
+
+	ci::gl::pushMatrices();
+	ci::gl::setViewport(mFbo.getBounds());
+	
+	ci::CameraOrtho _camera;
+	_camera.setOrtho(0, mFbo.getWidth(), mFbo.getHeight(), 0, 0.03f, 100.0f);
+	ci::gl::setMatrices(_camera);
+}
+
+ScopedFramebuffer::~ScopedFramebuffer()
+{
+	ci::gl::popMatrices();
+	ci::gl::setViewport(mOriginalViewport);
+	
+	mFbo.unbindFramebuffer();
+}
+
+}
